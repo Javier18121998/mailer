0c2cdffba05779a5481434c040beb592a614e1c6
Javier18121998 <Javier.Marin.Garcia.Linkedin01@hotmail.com>
2022-11-06 21:07:35 -0600

    struture funtion mmon to get service

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 import express  from "express";
 import path from 'path'
+import sgMail from '@sendgrid/mail'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
+sgMail.setApiKey(process.env.SGKEY)
 app.use(express.json())
 app.use(express.static('app'))
 app.get('/', (req, res) =>{
     res.sendFile(`${path.resolve()}/index.html`)
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+        to: '',
+        from: process.env.FROM,
+        subject: '',
+        text: '',
+        html: ''
+    }
+    sgMail
+    .send(msg)
+    .then(() =>{
+        console.log('Email sent')
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+})
+app.post('/send', async (req, res) =>{
+    const {to, subject, html} = req.body
+    const msg ={
+        to, 
+        from: process.env.FROM,
+        subject,
+        html
+    }
+    try{
+        await sgMail.send(msg)
+        res.sendStatus(204)
+    }catch (e){
+        const messages = e.response.body.errors.map(e =>e.message).join(' ')
+        res.status(400).send(messages)
+    }
 })
 app.listen(3000, ()=> console.log('la app está corriendo'))
